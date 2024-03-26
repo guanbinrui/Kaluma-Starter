e0ecb035cabff0243ab27d82ee5fd1c68e590486
guanbinrui <guanbinrui@mask.io>
2024-03-27 02:48:51 +0800

    chore: complete 03

--- src/exec-03.ts
+++ src/exec-03.ts
@@ -1,18 +1,41 @@
 // read analogy voltage
 
-import { ADC } from "adc";
+const PIN_ADC = 28;
+const PIN_LED1 = 21;
+const PIN_LED2 = 20;
+const PIN_LED3 = 19;
 
-const PIN28 = 28;
-
-const adc = new ADC(PIN28);
-
-function read() {
-  const voltage = adc.read();
-  console.log(voltage);
-}
+const MAX = 0.5;
+const MIN = 0;
+const NUM_OF_LEDS = 3;
+const TOLERANCE_1 = (MAX - MIN) / NUM_OF_LEDS;
+const TOLERANCE_2 = TOLERANCE_1 * 2;
 
 function main() {
-  setInterval(() => read(), 10);
+  pinMode(PIN_LED1, OUTPUT);
+  pinMode(PIN_LED2, OUTPUT);
+  pinMode(PIN_LED3, OUTPUT);
+
+  digitalWrite(PIN_LED1, HIGH);
+  digitalWrite(PIN_LED2, HIGH);
+  digitalWrite(PIN_LED3, HIGH);
+
+  setInterval(() => {
+    const voltage = analogRead(PIN_ADC);
+
+    digitalWrite(
+      PIN_LED1,
+      voltage >= MIN && voltage <= TOLERANCE_1 ? HIGH : LOW,
+    );
+    digitalWrite(
+      PIN_LED2,
+      voltage > TOLERANCE_1 && voltage <= TOLERANCE_2 ? HIGH : LOW,
+    );
+    digitalWrite(
+      PIN_LED3,
+      voltage > TOLERANCE_2 && voltage <= MAX ? HIGH : LOW,
+    );
+  }, 100);
 }
 
 main();
